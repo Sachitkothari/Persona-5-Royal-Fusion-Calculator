85883381313dbea07e3c3f491668049285119933
Sachitkothari <61432064+Sachitkothari@users.noreply.github.com>
2026-01-17 17:24:17 +0200

    Initial Commit

---
+++ merge.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "persona_merge_rules.hpp"
+
+std::string calulate(const std::string persona_A, const std::string persona_B)
+{
+    
+    return "sfa";
+}
